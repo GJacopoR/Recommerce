48ff369c99aa3c8cc33f91186e9f376b0ee1377f
Gennaro Jacopo Russo <jrusso@taal.it>
2022-05-26 18:30:27 +0200

    navbar fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Navbar from './Components/Navbar/component';
